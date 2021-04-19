c85c263c06274eff1e2eaee5e6eeb1bca4520a96
Taoorwell <taoorwell@gmail.com>
2021-04-19 10:45:24 +0200

    1904

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
         initial_learning_rate = 0.001
         lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
             initial_learning_rate=initial_learning_rate,
-            decay_steps=1000,
+            decay_steps=900,
             decay_rate=0.96,
             staircase=True
         )
