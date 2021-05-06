390a1ac6852299380ffafcd1f5bef197e4f68f95
Taoorwell <taoorwell@gmail.com>
2021-05-06 17:30:07 +0200

    large pre

--- large_pre.py
+++ large_pre.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 
 if __name__ == '__main__':
     path = r'../large_scale/'
-    width = 500
-    image_16 = get_raster(raster_path=path + '2020_sub.tif')
-    image_16 = image_16[:, :, :-1]
+    width = 256
+    image_16 = get_raster(raster_path=path + 'strip_2020_0.tif')
+    # image_16 = image_16[:, :, :-1]
     model = build_res_unet(input_shape=(width, width, 7))
-    model.load_weights('checkpoints/checkpoints/ckpt-1m_combined_log_cosine_aug_309')
+    model.load_weights('checkpoints/ckpt-1m_combined_log_cosine_aug_309')
     pre_images = np.zeros(((image_16.shape[0] // width) * width, (image_16.shape[1] // width) * width, 1),
                           dtype=np.float32)
     # image_20 = get_raster(raster_path=path + '2020_sub.tif')
@@ -18,8 +18,8 @@ if __name__ == '__main__':
             image = image_16[r*width: (r+1)*width, c*width: (c+1)*width, :]
             pre = model.predict(np.expand_dims(image, axis=0))
             pre_images[r*width: (r+1)*width, c*width: (c+1)*width, :] = pre
-    plt.imshow(pre_images[:, :, 0])
-    plt.show()
-    write_geotiff(name='../large_scale/2020_pre.tif',
+    # plt.imshow(pre_images[:, :, 0])
+    # plt.show()
+    write_geotiff(name='../large_scale/strip_2020_0_pre_256.tif',
                   prediction=pre_images[:, :, 0],
-                  original_path=path + '2016_sub.tif')
+                  original_path=path + 'strip_2020_0.tif')
