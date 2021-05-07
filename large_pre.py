cba2f00adaf0903fbd28788c7bb9f1fed9680101
tao jiang <taoorwell@163.com>
2021-05-07 13:25:15 +0200

    predict subplots

--- large_pre.py
+++ large_pre.py
@@ -3,23 +3,23 @@ from residual_unet import *
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    path = r'../large_scale/'
-    width = 256
-    image_16 = get_raster(raster_path=path + 'strip_2020_0.tif')
-    # image_16 = image_16[:, :, :-1]
+    path = r'../large_scale/subplots/'
+    width = 250
+    subplots = sorted(glob(os.path.join(path, "*.tif")))
     model = build_res_unet(input_shape=(width, width, 7))
-    model.load_weights('checkpoints/ckpt-1m_combined_log_cosine_aug_309')
-    pre_images = np.zeros(((image_16.shape[0] // width) * width, (image_16.shape[1] // width) * width, 1),
-                          dtype=np.float32)
-    # image_20 = get_raster(raster_path=path + '2020_sub.tif')
-    for r in tqdm(range(image_16.shape[0] // width)):
-        for c in range(image_16.shape[1] // width):
-            # print([r*width, (r+1)*width, c*width, (c+1)*width])
-            image = image_16[r*width: (r+1)*width, c*width: (c+1)*width, :]
-            pre = model.predict(np.expand_dims(image, axis=0))
-            pre_images[r*width: (r+1)*width, c*width: (c+1)*width, :] = pre
-    # plt.imshow(pre_images[:, :, 0])
-    # plt.show()
-    write_geotiff(name='../large_scale/strip_2020_0_pre_256.tif',
-                  prediction=pre_images[:, :, 0],
-                  original_path=path + 'strip_2020_0.tif')
+    model.load_weights('checkpoints/checkpoints/ckpt-1m_combined_log_cosine_aug_309')
+    # print(subplots)
+    for plot in tqdm(subplots):
+        image = get_raster(raster_path=plot)
+        pre_images = np.zeros(((image.shape[0] // width) * width, (image.shape[1] // width) * width, 1),
+                              dtype=np.float32)
+        for r in tqdm(range(image.shape[0] // width)):
+            for c in range(image.shape[1] // width):
+                # print([r*width, (r+1)*width, c*width, (c+1)*width])
+                image1 = image[r*width: (r+1)*width, c*width: (c+1)*width, :]
+                pre = model.predict(np.expand_dims(image1, axis=0))
+                pre_images[r*width: (r+1)*width, c*width: (c+1)*width, :] = pre
+        write_geotiff(name=path + plot.split('\\')[-1].split('.')[0] + '_pre.tif',
+                      prediction=pre_images[:, :, 0],
+                      original_path=plot)
+        print('Prediction file check: {}'.format(path + plot.split('\\')[-1].split('.')[0] + '_pre.tif'))
