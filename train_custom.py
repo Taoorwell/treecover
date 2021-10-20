bc53e7732cf5b9191a098a39053228c9288c386f
Taoorwell <taoorwell@gmail.com>
2021-10-20 17:11:31 +0200

    delete something

--- train_custom.py
+++ train_custom.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     width = 256
     train_batch_size = 8
     valid_batch_size = 10
-    epochs = 500
+    epochs = 300
     initial_learning_rate = 0.0001
     loss_fn = dice_loss
     # train datasets
@@ -28,13 +28,13 @@ if __name__ == '__main__':
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         # model = build_res_unet(input_shape=(width, width, 7))
-        model = U_Net(input_shape=(width, width, 7), n_classes=1, recurrent=False, residual=True, attention=False)
+        model = U_Net(input_shape=(width, width, 7), n_classes=1, recurrent=False, residual=True, attention=True)
         model.compile(optimizer=optimizer, loss=[loss_fn], metrics=[iou])
     model.summary()
 
     learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_cosine_decay, verbose=0)
     # tensorboard
-    tensorboard_callbacks = tf.keras.callbacks.TensorBoard(log_dir='tb_callback_dir/unet_res_300',
+    tensorboard_callbacks = tf.keras.callbacks.TensorBoard(log_dir='tb_callback_dir/unet_res_att_300',
                                                            histogram_freq=1)
 
     model.fit(train_datasets,
@@ -42,6 +42,6 @@ if __name__ == '__main__':
               epochs=epochs,
               validation_data=valid_datasets,
               validation_steps=len(valid_datasets),
-              callbacks=[learning_rate_scheduler])
+              callbacks=[learning_rate_scheduler, tensorboard_callbacks])
     # model.save('model.h5')
-    model.save_weights('checkpoints/ckpt-unet_res_300')
+    model.save_weights('checkpoints/ckpt-unet_res_att_300')
