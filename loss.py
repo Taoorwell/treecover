b348977308862da0ba96f85543586cc1cad13c0f
Taoorwell <taoorwell@gmail.com>
2021-10-25 09:37:25 +0200

    25102021

--- loss.py
+++ loss.py
@@ -91,13 +91,13 @@ def log_conv(y_true):
 
 
 # combine cross entropy and focal tversky loss
-def combined_loss(y_true, y_pred, weight=True):
+def combined_loss(y_true, y_pred, weight=False):
     loss = cross_entropy(y_true, y_pred, weight=weight) + 0.5 * focal_tversky_loss(y_true, y_pred)
     return loss
 
 
 # combine cross entropy and logarithm of iou
-def combined_log_loss(y_true, y_pred, weight=True):
+def combined_log_loss(y_true, y_pred, weight=False):
     eps = 1E-15
     loss = cross_entropy(y_true, y_pred, weight=weight) - tf.math.log(iou(y_true, y_pred) + eps)
     return loss
