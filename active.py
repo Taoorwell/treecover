398f534c0404386e69a89e96a18e60b5adbcd720
Taoorwell <taoorwell@gmail.com>
2021-10-28 17:31:23 +0200

    active training

--- active.py
+++ active.py
@@ -1,6 +1,8 @@
+import math
+import tensorflow as tf
 import pandas as pd
 import numpy as np
-from loss import dice, iou
+from loss import dice_loss, iou, tree_iou
 from dataloder import get_path, dataset
 from unets import U_Net
 from test_pre import predict_on_array
@@ -9,29 +11,91 @@ if __name__ == '__main__':
     # parameter define
     width = 256
     path = r'../quality/high'
+    n_classes = 2
+    train_batch_size = 4
+    valid_batch_size = 10
     n_inference = 10
     eps = 10e-10
-
-    # trained model reload
-    model = U_Net(input_shape=(256, 256, 7), n_classes=2, recurrent=False, residual=True, attention=False)
-    model.load_weights(r'checkpoints/ckpt-unet_res_softmax_dice_4_500')
-    print('model load successfully')
-
-    # datasets preparation
+    initial_learning_rate = 0.0001
+    epochs = 100
+    # validation datasets always
+    image_path_valid, mask_path_valid, image_i_valid = get_path(path=path,
+                                                                mode='valid',
+                                                                seed=1,
+                                                                active=0)
+    valid_datasets = dataset(image_path_valid,
+                             mask_path_valid,
+                             mode='valid',
+                             image_shape=(width, width),
+                             batch_size=valid_batch_size,
+                             n_classes=n_classes)
+    print('Validation datasets loading')
+    print(len(image_i_valid), image_i_valid)
+    # active datasets preparation for training
     image_path_active1, mask_path_active1, image_id_active1 = get_path(path=path,
                                                                        mode='train',
                                                                        seed=1,
                                                                        active=1)
+
     active1_datasets = dataset(image_path_active1,
                                mask_path_active1,
+                               mode='train',
+                               image_shape=(width, width),
+                               batch_size=train_batch_size,
+                               n_classes=n_classes)
+    print('active train datasets loading')
+    print(len(image_id_active1), image_id_active1)
+    # train model
+
+    optimizer = tf.optimizers.Adam(learning_rate=initial_learning_rate)
+
+    def lr_cosine_decay(e):
+        cosine_decay = 0.5 * (1 + math.cos(math.pi * e / epochs))
+        return initial_learning_rate * cosine_decay
+
+
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        # model = build_res_unet(input_shape=(width, width, 7))
+        model = U_Net(input_shape=(width, width, 7),
+                      n_classes=n_classes,
+                      dropout=.5,
+                      residual=True)
+        model.compile(optimizer=optimizer, loss=[dice_loss], metrics=[iou, tree_iou])
+    model.summary()
+
+    learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_cosine_decay, verbose=0)
+    # tensorboard
+    tensorboard_callbacks = tf.keras.callbacks.TensorBoard(log_dir='tb_callback_dir/unet_res_active1',
+                                                           histogram_freq=1)
+    print('active seed model training')
+    model.fit(active1_datasets,
+              steps_per_epoch=len(active1_datasets),
+              epochs=epochs,
+              validation_data=valid_datasets,
+              validation_steps=len(valid_datasets),
+              callbacks=[learning_rate_scheduler, tensorboard_callbacks])
+    # model.save('model.h5')
+    model.save_weights('checkpoints/ckpt-unet_res_active1')
+    # model.load_weights(r'checkpoints/ckpt-unet_res_softmax_dice_4_500')
+    print('active seed model save successfully')
+
+    # uncertainty datasets preparation
+    image_path_active2, mask_path_active2, image_id_active2 = get_path(path=path,
+                                                                       mode='train',
+                                                                       seed=1,
+                                                                       active=2)
+    active2_datasets = dataset(image_path_active2,
+                               mask_path_active2,
                                mode='test',
                                image_shape=(256, 256),
                                batch_size=1,
                                n_classes=2)
+    print('Uncertainty prediction')
 
     e1, e2, var = [], [], []
     # model prediction
-    for image_arr, mask_arr in active1_datasets:
+    for image_arr, mask_arr in active2_datasets:
         image_arr, mask_arr = image_arr[0], mask_arr[0]
         # print(image_arr.shape, mask_arr.shape)
         outputs = np.zeros((n_inference, ) + mask_arr.shape, dtype=np.float32)
